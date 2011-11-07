bddd1b70582d2c69adee0b82c4604010ba32f1fc
Mark Madsen <mark@agileanimal.com>
2011-11-06 23:53:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProLit::Application.config.secret_token = 'bada3a1a36fd5bb770a074a7a7d842f44bcccd13e43ce368eba777671657d955074f51cd76560214088a96b803aac9bdd89793c06aa7aefd5bbc403a521ac1f4'
